fef241ab7e6b99de88639c32eecf06850ba65a3d
ibrahimsugun <1150606905@nku.edu.tr>
2022-11-16 01:44:26 +0300

    asiankitchen

---
+++ asianKitchen/asianKitchenSvelte/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import CardItem from "./lib/cardItem.svelte";
+  import CardData from "./lib/menu";
+
+  const buttons = ["All", "Korea", "Japan", "China"];
+
+  let filter = "All";
+
+  $: items =
+    filter == "All"
+      ? CardData
+      : CardData.filter((item) => item.category == filter);
+</script>
+
+<main>
+  <div class="container">
+    <section class="menu">
+      <!-- title -->
+      <div class="title">
+        <h2>Asian Kitchen's Menu</h2>
+      </div>
+      <!-- filter buttons-->
+      <div class="btn-container">
+        {#each buttons as button}
+          <button
+            class="btn btn-outline-dark btn-item"
+            on:click={() => (filter = button)}
+          >
+            {button}
+          </button>
+        {/each}
+      </div>
+      <!-- menu items -->
+      <div id="items" class="section-center row">
+        {#each items as item}
+          <CardItem {...item} />
+        {/each}
+      </div>
+    </section>
+  </div>
+</main>
---
+++ asianKitchen/asianKitchenSvelte/src/lib/cardItem.svelte
@@ -0,0 +1,10 @@
+<div class="menu-items col-lg-6 col-sm-12">
+  <img src={$$props.img} alt={$$props.title} class="photo" />
+  <div class="menu-info">
+    <div class="menu-title">
+      <h4>{$$props.title}</h4>
+      <h4 class="price">{$$props.price}</h4>
+    </div>
+    <div class="menu-text">{$$props.desc}</div>
+  </div>
+</div>
